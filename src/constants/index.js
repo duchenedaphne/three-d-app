48999b38415e34098e2bbb95ab7efe711e3762ad
duchenedaphne <duchenedaphne@outlook.com>
2025-03-14 15:17:21 +0100

    deployment

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,22 +3,22 @@ export const navLinks = [
     {
         id: 1,
         name: 'Home',
-        href: '#home',
+        href: '/',
     },
     {
         id: 2,
         name: 'About',
-        href: '#about',
+        href: 'https://github.com/duchenedaphne',
     },
     {
         id: 3,
         name: 'Work',
-        href: '#work',
+        href: '/',
     },
     {
         id: 4,
         name: 'Contact',
-        href: '#contact',
+        href: 'https://www.linkedin.com/in/duchenedaphne/',
     },
 ];
 
